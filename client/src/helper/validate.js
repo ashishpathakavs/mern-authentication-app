96115a23be70085d09ab48281a24408705351a70
ashishpathakavs <ashish.pathak@byjus.com>
2023-04-20 09:05:41 +0530

    basic template created

---
+++ client/src/helper/validate.js
@@ -0,0 +1,46 @@
+import toast from "react-hot-toast";
+
+/** validate login page username */
+export async function usernameValidate(values) {
+  const errors = usernameVerify({}, values);
+
+  return errors;
+}
+
+/** validate password */
+export async function passwordValidate(values) {
+  const errors = passwordVerify({}, values);
+
+  return errors;
+}
+
+/** password  verify*/
+function passwordVerify(errors = {}, values) {
+  /* eslint-disable no-useless-escape */
+  const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+
+  if (!values.password) {
+    errors.password = toast.error("Password Required...!");
+  } else if (values.password.includes(" ")) {
+    errors.password = toast.error("Wrong Password...!");
+  } else if (values.password.length < 4) {
+    errors.password = toast.error(
+      "Password must be more than 4 characters long"
+    );
+  } else if (!specialChars.test(values.password)) {
+    errors.password = toast.error("Password must have special character");
+  }
+
+  return errors;
+}
+
+// validate username
+const usernameVerify = (error = {}, values) => {
+  if (!values.username) {
+    error.username = toast.error("Username Required...!");
+  } else if (values.username.includes(" ")) {
+    error.username = toast.error("Invalid Username...!");
+  }
+
+  return error;
+};
